d9e814262785e3f88e729e7e04f7feab87f90309
dreamforSh <3856889129@qq.com>
2026-05-07 17:04:09 +0800

    init

---
+++ src/main/java/com/github/foxy/Foxy.java
@@ -0,0 +1,11 @@
+package com.github.foxy;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Foxy.MODID)
+public class Foxy {
+    public static final String MODID = "foxy";
+
+    public Foxy() {
+    }
+}
